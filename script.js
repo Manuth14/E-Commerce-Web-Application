5412ff4f0db18875f9b6a1b5a6f3484ddfbd66ef
Manuth Thejaka <129110382+Manuth14@users.noreply.github.com>
2023-06-21 10:10:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function changeView() {
+    var signUpBox = document.getElementById("signupbox");
+    var signInBox = document.getElementById("signinbox");
+
+    signUpBox.classList.toggle("d-none");
+    signInBox.classList.toggle("d-none");
+}
+
+function Users() {
+    var productbox = document.getElementById("productbox");
+    var userbox = document.getElementById("userbox");
+
+    productbox.classList.toggle("d-none");
+    userbox.classList.toggle("d-none");
+
+}
